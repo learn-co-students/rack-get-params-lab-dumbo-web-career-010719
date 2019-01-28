f9ff4be175daabd6c7e68d450f32229733bc981b
Daniel Toma <danieltoma@ubalumni.com>
2019-01-28 10:33:49 -0500

    committing for kicks

--- app/application.rb
+++ app/application.rb
@@ -1,38 +1,3 @@
-# class Application
-#
-#   @@items = ["Apples","Carrots","Pears"]
-#
-#   @@cart = []
-#
-#   def call(env)
-#     resp = Rack::Response.new
-#     req = Rack::Request.new(env)
-#
-#     if req.path.match(/items/)
-    #   @@items.each do |item|
-    #     resp.write "#{item}\n"
-    #   end
-    # elsif req.path.match(/search/)
-    #   search_term = req.params["q"]
-    #   resp.write handle_search(search_term)
-    # elsif req.path.match(/cart/)
-    #   @@cart.each do |thing|
-    #     resp.write "#{thing}\n"
-    #   end
-    # elsif req.path.match(/add/)
-    #   add_term = req.params["item"]
-    #
-    #   if @@items.include?(add_term)
-    #     @@cart << add_term
-    #     resp.write "Found #{add_term}"
-    #   else
-    #     resp.write "Couldn't find #{add_term}"
-    #   end
-    # else
-    #   resp.write "Path Not Found"
-    # end
-
-#
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
